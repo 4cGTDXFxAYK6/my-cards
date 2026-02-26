08a5cd1c80f11ce00924a83069325e7700800bd7
4cGTDXFxAYK6 <species05618@gmail.com>
2026-02-26 10:16:12 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,41 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('v1').then((cache) => cache.addAll(['index.html'])));
+const CACHE_NAME = "mycards-alpha-v1";
+
+const urlsToCache = [
+  "/mycards-alpha/",
+  "/mycards-alpha/index.html",
+  "/mycards-alpha/manifest.json",
+  "/mycards-alpha/icon-512.png"
+];
+
+// インストール
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+// 古いキャッシュ削除
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// fetch処理
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
 });
